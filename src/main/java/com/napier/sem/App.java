7480f3c69ab5b2bd27e579c0ddf97c8dcec62138
jimmyx99 <40414311@napier.ac.uk>
2019-08-08 13:48:38 +0630

    lab 2 new

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println("yah boo!");
+        System.out.println("Yah boo!");
+        System.out.println("Boo yah!");
     }
 }    
